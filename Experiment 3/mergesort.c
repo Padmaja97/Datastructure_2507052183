cf6a88e62bfb7873679674471a604da1e28cf994
Padmaja97 <padmajathakre307@gmail.com>
2026-04-07 10:34:44 +0530

    Add mergesort implementation in mergesort.c
    Implement mergesort algorithm with merge function.

---
+++ Experiment 3/mergesort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Function to merge two subarrays
+void merge(int arr[], int left, int mid, int right) {int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge temp arrays back into arr[left..right]
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements of L[]
+    while (i < n1) {
+        arr[k] = L[i];
+ i++;
+        k++;
+    }
+
+    // Copy remaining elements of R[]
+    while (j < n2) {
